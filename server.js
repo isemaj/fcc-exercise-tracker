fab886f2cfb38f018ac4969152fc30d67ef02039
James Itum <39540851+isemaj@users.noreply.github.com>
2018-12-26 18:20:10 +0800

    remote mongodb package and add pug

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const mongodb = require("mongodb");
 const mongoose = require("mongoose");
 
 const app = express();
